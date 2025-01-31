8b6eaa31d659c8cf41422ac2871dc6dcb893e721
zahra-meyy <azzahrafaiqohnisa@gmail.com>
2025-01-31 15:01:21 +0700

    menambahkan page siswa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { useState } from "react";
 import Home from "./page/Home";
 import About from "./page/About";
 import Contact from "./page/Contact";
+import PageSiswa from "./Siswa/PageSiswa"; // Update path sesuai folder Siswa
 import "./App.css";
 
 function App() {
@@ -19,12 +19,16 @@ function App() {
           <li>
             <Link to="/contact">Contact</Link>
           </li>
+          <li>
+            <Link to="/siswa">Daftar Siswa</Link> {/* Link ke PageSiswa */}
+          </li>
         </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/siswa" element={<PageSiswa />} /> {/* Daftar siswa */}
       </Routes>
     </Router>
   );
