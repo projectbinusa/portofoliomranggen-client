6d93e1028bca053b100277ba986b6685515eeebd
lintang3108 <setiaalintang@gmail.com>
2025-01-31 11:51:39 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import Home from "./page/Home";
+import About from "./page/About";
+import Contact from "./page/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <nav className="nav-bar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
